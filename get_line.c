532a20ee12648221b19cc8423c187ebbd2d1dc41
Ikeh Victor <victorikeh77@gmail.com>
2022-11-17 04:54:29 -0500

    simple shell

--- get_line.c
+++ get_line.c
@@ -1,4 +1,4 @@
-#include "mainmainmainmainmainmainmainmain.h"
+#include "holberton.h"
 
 /**
  * bring_line - assigns the line var for get_line
--- get_sigint.c
+++ get_sigint.c
@@ -9,4 +9,3 @@ void get_sigint(int sig)
 	(void)sig;
 	write(STDOUT_FILENO, "\n^-^ ", 5);
 }
-
